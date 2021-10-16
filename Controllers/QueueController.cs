3a657d729ddb59e90832f4b31daf2472d951b827
veta1428 <lizavetka1428@gmail.com>
2021-10-16 12:22:22 +0300

    Add project files.

---
+++ Controllers/QueueController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EQueue.Controllers
+{
+    public class QueueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ViewResult GetQueueById(int queueId)
+        {
+            return View();
+        }
+    }
+}
